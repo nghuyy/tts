2ec54c8538661271577b258fcda460756b9bbbab
Huy Nguyễn <huyndx@outlook.com>
2022-01-19 10:41:59 +0700

    .

--- android/src/main/java/ngghuy/tts/tts/TtsPlugin.java
+++ android/src/main/java/ngghuy/tts/tts/TtsPlugin.java
@@ -1,20 +1,27 @@
 package ngghuy.tts.tts;
 
+import android.app.Activity;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.speech.tts.TextToSpeech;
+
 import androidx.annotation.NonNull;
 
+import java.util.Locale;
+
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
+import io.flutter.embedding.engine.plugins.activity.ActivityAware;
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 
 /** TtsPlugin */
-public class TtsPlugin implements FlutterPlugin, MethodCallHandler {
-  /// The MethodChannel that will the communication between Flutter and native Android
-  ///
-  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-  /// when the Flutter Engine is detached from the Activity
+public class TtsPlugin implements FlutterPlugin, MethodCallHandler, ActivityAware {
   private MethodChannel channel;
+  private TextToSpeech tts;
+  private Activity activity;
 
   @Override
   public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
@@ -24,15 +31,46 @@ public class TtsPlugin implements FlutterPlugin, MethodCallHandler {
 
   @Override
   public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
-    if (call.method.equals("getPlatformVersion")) {
-      result.success("Android " + android.os.Build.VERSION.RELEASE);
+    if(call.method.equals("init")){
+        if(call.argument("lang") != null){
+          initTTSEngineer("vi");
+        }
+      initTTSEngineer((String)call.argument("lang"));
     } else {
       result.notImplemented();
     }
   }
 
+  void initTTSEngineer(String lang) {
+    if (tts == null) tts = new TextToSpeech(activity, status -> {
+      if (status == TextToSpeech.SUCCESS) {
+        tts.setLanguage(new Locale(lang));
+      }
+    });
+  }
+
   @Override
   public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
     channel.setMethodCallHandler(null);
+  }
+
+  @Override
+  public void onAttachedToActivity(@NonNull ActivityPluginBinding binding) {
+
+  }
+
+  @Override
+  public void onDetachedFromActivityForConfigChanges() {
+
+  }
+
+  @Override
+  public void onReattachedToActivityForConfigChanges(@NonNull ActivityPluginBinding binding) {
+    this.activity = binding.getActivity();
+  }
+
+  @Override
+  public void onDetachedFromActivity() {
+
   }
 }
